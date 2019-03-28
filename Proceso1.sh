ee0f78f11b040992a8a55469c75369479a2d3fde
Edibys <49040452+Edibys@users.noreply.github.com>
2019-03-28 12:34:09 -0600

    Add files via upload

---
+++ Proceso1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "creado por EDI" 
+echo "Ingrese el No.1"
+read n1
+echo "Ingrese el No.2"
+read n2
+echo "Suma:"$(($n1+$n2))
+echo "Resta:"$(($n1-$n2))
+echo "Multiplicacion:"$(($n1*$n2))
